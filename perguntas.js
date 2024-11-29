1c9fde507cf40465b2835aeadf8da7c90a654aae
Paulo Guilherme <168849918+paulao4455@users.noreply.github.com>
2024-11-29 13:03:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é C sharp?',
+    'C sharp é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou.'
+)
+
+criaCartao(
+    'Física',
+    'O que significa a fórmula E=MC²?',
+    'E = energia, m = massa e c2 = velocidade da luz elevada ao quadrado.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comida em inglês?',
+    'Comida em inglês é FOOD'
+)
